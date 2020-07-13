813a99b49c41424546162a999f67880ce2df7996
Nora Gaughan <noragaughan@gmail.com>
2020-07-13 15:33:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ function destructivelyRemoveFirstKitten(name) {return kittens.shift(name)}
 function appendKitten(name) {return [...kittens, name]}
 function prependKitten(name) {return [name, ... kittens]} 
 function removeLastKitten(name) {return kittens.slice(0, kittens.length-1)} 
-function removeFirstKitten(name) {return 
+function removeFirstKitten(name) {return kittens.slice(1)}
