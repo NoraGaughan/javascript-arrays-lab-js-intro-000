b1aa073a36b49da9277a42eef03469c8082b7383
Nora Gaughan <noragaughan@gmail.com>
2020-07-13 15:28:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var kittens = ['Milo', 'Otis', 'Garfield']
 function destructivelyAppendKitten(name) {return kittens.push(name)}
-function destructivelyPrepend(name) {return kittens.unshift(name)}
+function destructivelyPrependKitten(name) {return kittens.unshift(name)}
